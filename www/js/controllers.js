8cb2ecdc5d5ce4d77d89af7ac224764b43623265
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2015-08-23 06:14:53 +0700

    commit

---
+++ www/js/controllers.js
@@ -0,0 +1,94 @@
+angular.module('starter.controllers', ['ngCookies'])
+
+.config(['$httpProvider', function($httpProvider) {
+    // $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    // $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    // $httpProvider.defaults.headers.post['X-CSRFToken'] = Cookies.get('csrftoken');
+    $httpProvider.defaults.withCredentials = true;
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    // $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}])
+.controller('DashCtrl', function($scope, $http) {
+
+  console.log(Cookies.get('csrftoken'));
+  $scope.postData = function() {
+    // $http.post('http://10.107.135.214:8000/api/user/login', {'csrfmiddlewaretoken':Cookies.get('csrftoken')} ).
+    //   success(function(data, status, headers, config) {
+    //     console.log('post deliveryDone berhasil');
+    //     alert('Success : Delivery has been done');
+    //   }).
+    //   error(function(data, status, headers, config) {
+    //     console.log('post storeData gagal');
+    //     console.log(data);
+    // });
+    var request = $http({
+        method: "post",
+        url: "http://10.107.135.214:8000/api/user/login",
+        data: {
+            username: 'test',
+            password: "test",
+            csrfmiddlewaretoken: Cookies.get('csrftoken')
+        },
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+    // Store the data-dump of the FORM scope.
+    request.success(
+        function( html ) {
+            // $scope.cfdump = html;
+            console.log(html);
+        }
+    );
+  };
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+})
+
+.controller('cekHargaPasar', function($scope, $http) {
+  $http.get('http://10.107.135.214:8000/api/province').then(function(resp) {
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+
+    $scope.provinces = resp.data.data;
+    console.log($scope.provinces);
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+})
+
+.controller('cekHargaKomoditas', function($scope, $http) {
+  $http.get('http://10.107.135.214:8000/api/commodities').then(function(resp) {
+    console.log('Success', resp);
+    // For JSON responses, resp.data contains the result
+
+    $scope.commodities = resp.data.data;
+    console.log($scope.commodities);
+  }, function(err) {
+    console.error('ERR', err);
+    // err.status will contain the status code
+  })
+})
+
+.controller('BookmarCtrl', function($scope, $http) {
+
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,68 @@
+angular.module('starter.services', [])
+.factory('CRFSService', function() {
+  return {
+    getCookie: function (name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+      }
+    }
+})
+.factory(
+    "transformRequestAsFormPost",
+    function() {
+        // I prepare the request data for the form post.
+        function transformRequest( data, getHeaders ) {
+            var headers = getHeaders();
+            headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
+            return( serializeData( data ) );
+        }
+        // Return the factory value.
+        return( transformRequest );
+        // ---
+        // PRVIATE METHODS.
+        // ---
+        // I serialize the given Object into a key-value pair string. This
+        // method expects an object and will default to the toString() method.
+        // --
+        // NOTE: This is an atered version of the jQuery.param() method which
+        // will serialize a data collection for Form posting.
+        // --
+        // https://github.com/jquery/jquery/blob/master/src/serialize.js#L45
+        function serializeData( data ) {
+            // If this is not an object, defer to native stringification.
+            if ( ! angular.isObject( data ) ) {
+                return( ( data == null ) ? "" : data.toString() );
+            }
+            var buffer = [];
+            // Serialize each key in the object.
+            for ( var name in data ) {
+                if ( ! data.hasOwnProperty( name ) ) {
+                    continue;
+                }
+                var value = data[ name ];
+                buffer.push(
+                    encodeURIComponent( name ) +
+                    "=" +
+                    encodeURIComponent( ( value == null ) ? "" : value )
+                );
+            }
+            // Serialize the buffer and clean it up for transportation.
+            var source = buffer
+                .join( "&" )
+                .replace( /%20/g, "+" )
+            ;
+            return( source );
+        }
+    }
+);
